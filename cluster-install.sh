07253d454fbe05dbaaf7a9b0fb39c1992c3758fa
reposforlabs <160736679+reposforlabs@users.noreply.github.com>
2024-03-29 18:07:27 +0530

    Create cluster-install.sh

---
+++ cluster-install.sh
@@ -0,0 +1,66 @@
+# Update the VMs
+sudo apt update
+sudo apt-upgrade -y
+sudo apt-get remove needrestart -y
+
+# Add entries in hosts file
+sudo sed -i '2i10.0.0.4 master' /etc/hosts
+sudo sed -i '3i192.168.0.4 node1' /etc/hosts
+sudo sed -i '4i192.168.0.5 node2' /etc/hosts
+
+# Turn off swap (Most cloud VMs don't have swap turned on by default, but still)
+sudo swapoff -a
+
+# Adjust kernel settings
+sudo tee /etc/modules-load.d/containerd.conf <<EOF
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+sudo tee /etc/sysctl.d/kubernetes.conf <<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+# Reload system
+sudo sysctl --system
+
+# Install a few utils
+sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable" -y
+
+# Update the system
+sudo apt update
+
+# Install container runtime
+sudo apt install -y containerd.io
+
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+
+# Install kubernetes
+curl -fsSL https://dl.k8s.io/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list -y
+sudo apt update
+sudo apt install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+# Configure Kubernetes Master Node
+sudo kubeadm init --control-plane-endpoint=master
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Install Calico Pod Network Add-on
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
