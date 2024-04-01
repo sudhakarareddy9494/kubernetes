c5b67b8bfb50d507ac8cdc41e814247c59861849
reposforlabs <160736679+reposforlabs@users.noreply.github.com>
2024-04-01 18:09:07 +0530

    Update cluster-install.sh

--- cluster-install.sh
+++ cluster-install.sh
@@ -49,11 +49,16 @@ sudo systemctl restart containerd
 sudo systemctl enable containerd
 
 # Install kubernetes
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes.gpg
-echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/kubernetes.gpg] http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list
-sudo apt update
-sudo apt install -y kubelet kubeadm kubectl
+sudo apt-get update
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
+
 
 # Configure Kubernetes Master Node
 sudo kubeadm init --control-plane-endpoint=master
